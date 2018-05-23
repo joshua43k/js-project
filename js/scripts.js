093a272b9aaa753b33b5f8dc2b92aa6f06bb69eb
joshua <josh43k@gmail.com>
2018-05-23 15:25:47 -0700

    day 1 var and log

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var myClass = "What i would like to get out this class to master javascript and also to finish something i could not finish a two years ago."
+// var and myName are called declarations
+
+console.log(myClass);
