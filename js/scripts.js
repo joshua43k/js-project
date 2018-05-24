6cf54d818bc2cc47e1abc9fecc9218d3756a12b4
joshua <josh43k@gmail.com>
2018-05-23 18:50:45 -0700

    day 1 var and log

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var myClass = "What i would like to get out this class to master javascript and also to finish something i could not finish a two years ago."
 var myHobby = "My main hobby is gaming"
-// var and myName are called declarations
-
 console.log(myClass);
 console.log(myHobby);
+//var _____ are called declarations
+//javascript is ment to carve data and save something
