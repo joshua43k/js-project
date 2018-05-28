d1f2f8c9b9a5d806202a792bc8d059deb1604b16
joshua <josh43k@gmail.com>
2018-05-28 13:43:11 -0700

    day 2 takehome gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var firstName = prompt("First Name");
 var lastName = prompt("Last Name");
-var birthDay = prompt("Date of birth");
+var birthDay = new Date(prompt("Date of birth"));
 
 console.log(firstName);
 alert(lastName);
