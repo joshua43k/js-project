75b9a4c6462f8b9cd7900bb7efbda5fd89168658
joshua <josh43k@gmail.com>
2018-05-29 16:05:53 -0700

    #

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-var firstName = prompt("First Name");
-var lastName = prompt("Last Name");
-var birthDay = new Date(prompt("Date of birth"));
-
-console.log(firstName);
-alert(lastName);
-confirm(birthDay)
